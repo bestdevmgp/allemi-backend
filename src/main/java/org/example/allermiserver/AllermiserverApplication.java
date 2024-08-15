7be4ca50a9c388b64995cd8984dc054d335a42ad
bestdevmgp <pmg3858@icloud.com>
2024-08-15 21:45:41 +0900

    Initial Commit

---
+++ src/main/java/org/example/allermiserver/AllermiserverApplication.java
@@ -0,0 +1,13 @@
+package org.example.allermiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllermiserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllermiserverApplication.class, args);
+	}
+
+}
